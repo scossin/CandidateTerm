59a657c99480b2a835fecebd519e248da75be554
Sebastien Cossin <sebastien.cossin@chu-bordeaux.fr>
2020-03-29 09:04:02 +0200

    fix bug case POS return null

--- CandidateTerms/src/main/java/fr/erias/candidateterms/candidateTerm/CandidateTermsDetectionTTPOS.java
+++ CandidateTerms/src/main/java/fr/erias/candidateterms/candidateTerm/CandidateTermsDetectionTTPOS.java
@@ -21,12 +21,12 @@ public class CandidateTermsDetectionTTPOS extends CandidateTermsDetection{
 	public CandidateTermsDetectionTTPOS(String[] posArray, String[] tokensArray) throws InvalidArraysLength {
 		super(posArray, tokensArray);
 	}
-	
+
 	/**
 	 * The key regular expression to extract candidate terms
 	 */
 	private final String regularE = "J*NJ*(PD?J*NJ*)*";
-	
+
 	/**
 	 * Set of rules specific to TreeTagger POS analyzer !!
 	 * @param posSequence An array of Part-of-speech
@@ -34,32 +34,33 @@ public class CandidateTermsDetectionTTPOS extends CandidateTermsDetection{
 	 */
 	protected boolean checkPOSsequence(String[] posSequence){
 		String pattern = "";
-        for(int i = 0; i < posSequence.length; i++){
-           if(posSequence[i].equals("DET:ART"))pattern=pattern+"D";
-           else if(posSequence[i].equals("NOM")|| posSequence[i].equals("NAM")|| posSequence[i].equals("ABR"))pattern=pattern+"N";
-           else if(posSequence[i].equals("NUM") || posSequence[i].equals("ADJ") || posSequence[i].equals("VER:pper"))pattern=pattern+"J";
-           else if(posSequence[i].equals("PRP") || posSequence[i].equals("PRP:det"))pattern=pattern+"P";
-           else pattern = pattern+"Z";
-        }
-        
-        if (matchPattern(pattern)){
-        	return(true);
-        } else {
-        	return(false);
-        }
+		for(int i = 0; i < posSequence.length; i++){
+			if(posSequence[i] == null)pattern=pattern+"Z";
+			else if(posSequence[i].equals("DET:ART"))pattern=pattern+"D";
+			else if(posSequence[i].equals("NOM")|| posSequence[i].equals("NAM")|| posSequence[i].equals("ABR"))pattern=pattern+"N";
+			else if(posSequence[i].equals("NUM") || posSequence[i].equals("ADJ") || posSequence[i].equals("VER:pper"))pattern=pattern+"J";
+			else if(posSequence[i].equals("PRP") || posSequence[i].equals("PRP:det"))pattern=pattern+"P";
+			else pattern = pattern+"Z";
+		}
+
+		if (matchPattern(pattern)){
+			return(true);
+		} else {
+			return(false);
+		}
 	}
-	
+
 	/**
 	 * If the sequence of Part-Of-Speech match the regular expression the function return true
 	 * @param pattern
 	 * @return
 	 */
 	private boolean matchPattern(String pattern){
-        if(Pattern.matches(regularE, pattern) && !pattern.equals("J")){
-    		return(true);
-        } else {
-        	return(false);
-        }
+		if(Pattern.matches(regularE, pattern) && !pattern.equals("J")){
+			return(true);
+		} else {
+			return(false);
+		}
 	}
 
 }
